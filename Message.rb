1f8659b430bc76b5040bfb31bcc5280a680ea8a8
hab278 <hab278@nyu.edu>
2017-01-21 17:16:15 -0500

    Create Message.rb

---
+++ Message.rb
@@ -0,0 +1,20 @@
+require './annotation'
+class Message
+  def initialize
+    @message = {
+      type: 'appMessage',
+      version: 1.0,
+      annotations: []
+    }
+  end
+
+  def text=(text)
+    annotation = Annotation.new
+    annotation.text = text
+    @message[:annotations] = [annotation]
+  end
+
+  def to_json(options = nil)
+    @message.to_json
+  end
+end
