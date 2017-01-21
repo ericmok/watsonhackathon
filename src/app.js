b4d22b6f97f25dc30d4ce2875de50752709e8031
Eric Mok <ericmok2@gmail.com>
2017-01-21 12:31:43 -0500

    Initial commit from hello world example

---
+++ src/app.js
@@ -0,0 +1,155 @@
+// A sample app that sends a 'hello world' message to a space in IBM
+// Watson Workspace
+
+// Usage:
+// npm run helloworld "space name" ["message text"]
+
+import * as request from 'request';
+import debug from 'debug';
+
+// Debug log
+const log = debug('watsonwork-helloworld-app');
+
+// Return the list of spaces the app belongs to
+const spaces = (tok, cb) => {
+  request.post('https://api.watsonwork.ibm.com/graphql', {
+    headers: {
+      jwt: tok,
+      'Content-Type': 'application/graphql'
+    },
+
+    // This is a GraphQL query, used to retrieve the list of spaces
+    // visible to the app (given the app OAuth token)
+    body: `
+      query {
+        spaces (first: 50) {
+          items {
+            title
+            id
+          }
+        }
+      }`
+  }, (err, res) => {
+    if(err || res.statusCode !== 200) {
+      log('Error retrieving spaces %o', err || res.statusCode);
+      cb(err || new Error(res.statusCode));
+      return;
+    }
+
+    // Return the list of spaces
+    const body = JSON.parse(res.body);
+    log('Space query result %o', body.data.spaces.items);
+    cb(null, body.data.spaces.items);
+  });
+};
+
+// Return an OAuth token for the app
+const token = (appId, secret, cb) => {
+  request.post('https://api.watsonwork.ibm.com/oauth/token', {
+    auth: {
+      user: appId,
+      pass: secret
+    },
+    json: true,
+    form: {
+      grant_type: 'client_credentials'
+    }
+  }, (err, res) => {
+    if(err || res.statusCode !== 200) {
+      log('Error getting OAuth token %o', err || res.statusCode);
+      cb(err || new Error(res.statusCode));
+      return;
+    }
+    cb(null, res.body.access_token);
+  });
+};
+
+// Send an app message to the conversation in a space
+const send = (spaceId, text, tok, cb) => {
+  request.post(
+    'https://api.watsonwork.ibm.com/v1/spaces/' + spaceId + '/messages', {
+      headers: {
+        Authorization: 'Bearer ' + tok
+      },
+      json: true,
+      // An App message can specify a color, a title, markdown text and
+      // an 'actor' useful to show where the message is coming from
+      body: {
+        type: 'appMessage',
+        version: 1.0,
+        annotations: [{
+          type: 'generic',
+          version: 1.0,
+
+          color: '#6CB7FB',
+          title: 'Sample Message',
+          text: text,
+
+          actor: {
+            name: 'from sample helloworld app',
+            avatar: 'https://avatars1.githubusercontent.com/u/22985179',
+            url: 'https://github.com/watsonwork-helloworld'
+          }
+        }]
+      }
+    }, (err, res) => {
+      if(err || res.statusCode !== 201) {
+        log('Error sending message %o', err || res.statusCode);
+        cb(err || new Error(res.statusCode));
+        return;
+      }
+      log('Send result %d, %o', res.statusCode, res.body);
+      cb(null, res.body);
+    });
+};
+
+// Main app entry point
+// Send a message to the conversation in the space matching the given name
+export const main = (name, text, appId, secret, cb) => {
+  // Get an OAuth token for the app
+  token(appId, secret, (err, tok) => {
+    if(err) {
+      cb(err);
+      return;
+    }
+
+    // List the spaces the app belongs to
+    spaces(tok, (err, slist) => {
+      if(err) {
+        cb(err);
+        return;
+      }
+
+      // Find a space matching the given name
+      const space = slist.filter((s) => s.title === name)[0];
+      if(!space) {
+        cb(new Error('Space not found'));
+        return;
+      }
+
+      // Send the message
+      log('Sending \'%s\' to space %s', text, space.title);
+      send(space.id,
+        text || 'Hello World! Welcome to **Watson Work**!',
+        tok, (err, res) => {
+          if(err) {
+            cb(err);
+            return;
+          }
+          log('Sent message to space %s', space.title);
+          cb(null);
+        });
+    });
+  });
+};
+
+if(require.main === module)
+  // Run the app
+  main(process.argv[2], process.argv[3],
+    // Expect the app id and secret to be configured in env variables
+    process.env.HELLOWORLD_APP_ID, process.env.HELLOWORLD_APP_SECRET,
+    (err) => {
+      if(err)
+        console.log('Error sending message:', err);
+    });
+
---
+++ src/test/test.js
@@ -0,0 +1,129 @@
+// A sample app that sends a 'hello world' message to a space in IBM
+// Watson Workspace
+
+// Test the happy path
+
+import { expect } from 'chai';
+
+// Rudimentary mock of the request module
+let postspy;
+require('request');
+require.cache[require.resolve('request')].exports = {
+  post: (uri, opt, cb) => postspy(uri, opt, cb)
+};
+
+// Load the HelloWorld app
+const helloworld = require('../app');
+
+describe('watsonwork-helloworld', () => {
+  it('sends a hello world message to a space', (done) => {
+
+    // Check async callbacks
+    let checks = 0;
+    const check = () => {
+      if(++checks === 4)
+        done();
+    };
+
+    postspy = (uri, opt, cb) => {
+      // Expect a call to get an OAuth token for the app
+      if(uri === 'https://api.watsonwork.ibm.com/oauth/token') {
+        expect(opt.auth).to.deep.equal({
+          user: 'testappid',
+          pass: 'testsecret'
+        });
+        expect(opt.json).to.equal(true);
+        expect(opt.form).to.deep.equal({
+          grant_type: 'client_credentials'
+        });
+
+        // Return OAuth token
+        setImmediate(() => cb(undefined, {
+          statusCode: 200,
+          body: {
+            access_token: 'testaccesstoken'
+          }
+        }));
+        check();
+        return;
+      }
+
+      // Expect a call to GraphQL to retrieve a list of spaces
+      if(uri === 'https://api.watsonwork.ibm.com/graphql') {
+        expect(opt.headers).to.deep.equal({
+          jwt: 'testaccesstoken',
+          'Content-Type': 'application/graphql'
+        });
+        expect(opt.body.replace(/\s/g, '')).to.equal(`
+          query {
+            spaces (first: 50) {
+              items {
+                title
+                id
+              }
+            }
+          }`.replace(/\s/g, ''));
+
+        setImmediate(() => cb(undefined, {
+          statusCode: 200,
+          // Return list of spaces
+          body: JSON.stringify({
+            data: {
+              spaces: {
+                items: [{
+                  title: 'Test Space',
+                  id: 'testspace'
+                }]
+              }
+            }
+          })
+        }));
+        check();
+        return;
+      }
+
+      // Expect a call to send a message to the test space
+      if(uri ===
+        'https://api.watsonwork.ibm.com/v1/spaces/testspace/messages') {
+        expect(opt.headers).to.deep.equal({
+          Authorization: 'Bearer testaccesstoken'
+        });
+        expect(opt.json).to.equal(true);
+        expect(opt.body).to.deep.equal({
+          type: 'appMessage',
+          version: 1.0,
+          annotations: [{
+            type: 'generic',
+            version: 1.0,
+
+            color: '#6CB7FB',
+            title: 'Sample Message',
+            text: 'Hey',
+
+            actor: {
+              name: 'from sample helloworld app',
+              avatar: 'https://avatars1.githubusercontent.com/u/22985179',
+              url: 'https://github.com/watsonwork-helloworld'
+            }
+          }]
+        });
+        setImmediate(() => cb(undefined, {
+          statusCode: 201,
+          // Return list of spaces
+          body: {
+          }
+        }));
+        check();
+      }
+    };
+
+    // Run the HelloWorld app
+    helloworld.main('Test Space', 'Hey', 'testappid', 'testsecret',
+      (err, res) => {
+        // Expect a successful run
+        expect(err).to.equal(null);
+        check();
+      });
+  });
+});
+
